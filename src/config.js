b7e5fd629385523cab7bb520f8710cd4392342b7
Aleksandr Gavrilov <alg@digt.ru>
2017-10-25 19:45:02 +0300

    fix firebase config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase'
 
 export const appName = 'advreact-1610'
+const appId = 'ec352'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB31xpTtp4Jln_hb2kAbE4PGf6Mi8EgLyA",
-    authDomain: `${appName}.firebaseapp.com`,
-    databaseURL: `https://${appName}.firebaseio.com`,
-    projectId: appName,
-    storageBucket: "",
-    messagingSenderId: "397157634637"
+    apiKey: "AIzaSyB3LVTO7RSDrZAkHBkpzg9T5KkuoCoy4qo",
+    authDomain: `${appName}-${appId}.firebaseapp.com`,
+    databaseURL: `https://${appName}-${appId}.firebaseio.com`,
+    projectId: `${appName}-${appId}`,
+    storageBucket: `${appName}-${appId}.appspot.com`,
+    messagingSenderId: "651090769196"
 })
